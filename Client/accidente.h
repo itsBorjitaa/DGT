a29b7df598cd233f59a62e73d63a4e60896f1a08
IgorBarredo <igor.barredo@opendeusto.es>
2025-05-29 09:38:23 +0200

    Parte 3 estructura de datos c++ Cliente

---
+++ Client/accidente.h
@@ -0,0 +1,31 @@
+#ifndef ACCIDENTE_H
+#define ACCIDENTE_H
+
+class Accidente
+{
+private:
+	char* usuario;
+    char* fecha;
+    char* descripcion;
+public:
+	Accidente();
+	Accidente(const char*, const char*, const char*);
+	//Accidente(const Accidente&);      // NO QUEREMOS QUE SE PUEDA COPIAR
+	~Accidente();
+
+	char* getUsuario() const;
+	void setUsuario(const char*);
+	char* getFecha() const;
+	void setFecha(const char*);
+	char* getDescripcion() const;
+	void setDescripcion(const char*);
+
+    void consultarDatosAccidente(int idAccidente) const;
+	void consultarAccidentesUsuario(char *usuario) const;
+	int obtenerAnioActual() const;
+    int esAnioBisiesto(int anio) const;
+    int validarFecha(const char *fecha) const;
+    void registrarAccidenteConValidacion() const;
+};
+
+#endif /* ACCIDENTE_H_ */
--- accidente.h
+++ accidente.h
@@ -5,26 +5,4 @@ void consultarDatosAccidente();
 void consultarAccidentesUsuario(char *usuario);
 void registrarAccidenteConValidacion();
 
-class accidente
-{
-private:
-	Miembro* conyugeH;
-	Miembro* conyugeM;
-public:
-	Familia();
-	Familia(Miembro*, Miembro*);
-	Familia(const Familia&);
-	virtual ~Familia();
-
-	void setConyugeH(Miembro*);
-	Miembro* getConyugeH() const;
-	void setConyugeM(Miembro*);
-	Miembro* getConyugeM() const;
-
-	char* getNombre() const;
-	virtual void imprimir();
-	virtual int contarMenoresEdad(int);
-};
-
-
 #endif
